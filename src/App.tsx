49bf926f0a85ddf61813f9efc3757304937e70bd
Braian <00.braian.dev@gmail.com>
2020-11-15 04:30:03 -0300

    Create Menu Item
    Creating using dinamyc styling with styled-components and typescript interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import FeaturedCard from "./components/FeaturedCard";
+import MenuItem from './components/MenuItem'
 
 function App() {
   return (
     <div className="App" style={{ background: "#F2F2F2" }}>
-      <FeaturedCard />
+      <MenuItem />
     </div>
   );
 }
---
+++ src/components/MenuItem/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  selected: boolean;
+}
+
+const StyledMenuItem = styled.div`
+  height: 51px;
+  width: 100px;
+  left: 0px;
+  top: 0px;
+  border-radius: 10px;
+  background-color: ${(props: Props) => (props.selected ? `#FAD424` : `#fff`)};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+const MenuItem = ({ selected = true }) => {
+  return <StyledMenuItem selected={selected}>Hello</StyledMenuItem>;
+};
+
+export default MenuItem;
